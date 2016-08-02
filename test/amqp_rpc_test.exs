f24f1cb7deaa4ee232b60d9b80d25051e721a208
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-03 02:16:15 +0300

    basic test

--- test/amqp_rpc_test.exs
+++ test/amqp_rpc_test.exs
@@ -1,8 +1,60 @@
 defmodule AmqpRpcTest do
   use ExUnit.Case
-  doctest AmqpRpc
 
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  @tag timeout: 120000
+  test "Basic all-in-one test" do
+    {:ok, client_pid} = Fibonacci.start_link
+    assert Fibonacci.fib(5) == :timeout
+    spawn fn -> Fibonacci.fib(5) end
+    spawn fn -> Fibonacci.fib(5) end
+    spawn fn -> Fibonacci.fib(5) end
+
+    :timer.sleep(6000)
+    assert Fibonacci.fib(5) == :blown
+    :timer.sleep(61000)
+
+    {:ok, server_pid} = FibonacciServer.start_link
+    assert Fibonacci.fib(5) == 5
+
+    :erlang.exit(client_pid, :ok)
+    :erlang.exit(server_pid, :ok)
+  end
+end
+
+defmodule Fibonacci do
+  use AMQP.RPC.Client, [exchange: "",
+                        queue: "rpc_queue"]
+
+  def fib(n, timeout \\ @timeout) do
+    rpc(%{command_name: "fib",
+          args: n}, timeout)
+  end
+end
+
+defmodule FibonacciServer do
+  use AMQP.RPC.Server, [exchange: "",
+                        queue: "rpc_queue",
+                        commands: [:fib]]
+
+  ## adapted from https://gist.github.com/stevedowney/2f910bd3d72678b4cf99
+
+  def fib(0), do: 0
+
+  def fib(n)
+  when is_number(n) and n > 0,
+    do: fib(n, 1, 0, 1)
+
+  def fib(_), do: [error: "positive integers only"]
+
+  def fib(n, m, _prev_fib, current_fib)
+  when n == m,
+    do: current_fib
+
+  def fib(n, m, prev_fib, current_fib),
+    do: fib(n, m+1, current_fib, prev_fib + current_fib)
+
 end
