09fbe1cfa188078e60962d93f4d65f50ac6ca2b9
Ilya Khaprov <dead.trickster@gmail.com>
2016-08-06 19:56:08 +0300

    folsom stat plugin

---
+++ lib/amqp/rpc/stat/folsom.ex
@@ -0,0 +1,15 @@
+defmodule AMQP.RPC.Stat.Folsom do
+  @behaviour AMQP.RPC.Stat.Plugin
+
+  def init(name, opts) do
+    for result <- [:ok, :blown, :timeout], do: :folsom_metric.new_histogram(metric(name, result))
+  end
+
+  def instrument_request(name, _command_name, result, ms) do
+    :folsom_metrics.notify({metric(name, result), ms})
+  end
+
+  defp metric(name, :blown), do: name <> "$.blown"
+  defp metric(name, :timeout), do: name <> "$.timeout"
+  defp metric(name, _), do: name <> "$.ok"
+end
